4521ee4b0782c50f525950c303477888bbdbc07e
[happykala] <[1030775250@qq.com]>
2017-03-23 23:44:16 +0800

    输出node的全局变量，判断js代码的运行环境

--- hello/main.js
+++ hello/main.js
@@ -3,4 +3,13 @@ var hello = require('./hello');
 var s = 'lifeng';
 hello.greet(s);
 hello.getPostInfo(s);
-// global.console;
+//全局变量
+console.log(global.console);
+// console.log(process);
+//判断当前js代码的运行环境,在浏览器端的全局变量是window,在代码中判断window的类型如果是没有定义的那么js代码就不是在浏览器环境下运行的
+if(typeof(window) == 'undefined'){
+    console.log('node env');
+}else{
+    console.log('browser');
+}
+
